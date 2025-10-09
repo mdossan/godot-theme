5780ba8715e72d8cea3d2651e3d7df3c0b21c40f
mdos-san <mdossan@protonmail.com>
2025-10-09 20:09:25 +0200

    feat: create the ThemeBuilder node

---
+++ addons/theme/theme_builder.gd
@@ -0,0 +1,27 @@
+@tool
+class_name ThemeBuilder extends Node
+
+@export var margin_size: int = 16
+@onready @export var atlas_sprite: Texture2D = load("res://addons/theme/default_atlas.png"):
+	set(new_atlas):
+		print("Atlas has been loaded")
+		atlas_sprite = new_atlas
+@export_tool_button("Build Theme", "Callable") var button = build
+
+func build() -> void:
+	print("Starting theme build")
+	var theme_elements = ["primary", "hover", "selected", "disabled"]
+	for i in len(theme_elements):
+		var element_name = theme_elements[i]
+		var atlas_texture = AtlasTexture.new()
+		atlas_texture.atlas = atlas_sprite
+		atlas_texture.region = Rect2(48 * i, 0, 48, 48)
+		DirAccess.remove_absolute("res://addons/theme/%s.tres" % element_name)
+		ResourceSaver.save(atlas_texture, "res://addons/theme/%s.tres" % element_name)
+		var style_box: StyleBoxTexture = ResourceLoader.load("res://addons/theme/%s_box.tres" % element_name)
+		style_box.texture_margin_bottom = margin_size
+		style_box.texture_margin_left = margin_size
+		style_box.texture_margin_top = margin_size
+		style_box.texture_margin_right = margin_size
+	EditorInterface.get_resource_filesystem().scan()
+	print("Theme is builded")
